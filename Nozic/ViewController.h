76947f151c9c79895cae2e2247725ce2a648501e
Troy <731467114@qq.com>
2017-03-12 00:36:47 +0800

    Initial Commit

---
+++ Nozic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Nozic
+//
+//  Created by Troy-Wu on 12/03/2017.
+//  Copyright © 2017 com.LittleTroy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Nozic/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Nozic
+//
+//  Created by Troy-Wu on 12/03/2017.
+//  Copyright © 2017 com.LittleTroy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
