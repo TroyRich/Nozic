7da3dd547c5914cf488dcc610994a63686d4a083
Troy <731467114@qq.com>
2017-03-14 01:07:11 +0800

    工具栏的大概显示

--- Nozic/WYXMainViewController.m
+++ Nozic/WYXMainViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "WYXMainViewController.h"
+#import <Masonry.h>
 
 @interface WYXMainViewController ()
 
@@ -31,6 +32,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self addButtomBtn];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -38,23 +40,57 @@
     // Dispose of any resources that can be recreated.
 }
 
+//创建配置好的数据
+-(void)createTableView{
+    
+}
+
 //添加底部按钮
 -(void)addButtomBtn
 {
     self.saveBtn = [[UIBarButtonItem alloc] initWithTitle:@"保存" style:UIBarButtonItemStyleDone target:self action:@selector(saveBtnDidTouch:)];
-    self.playBtn = [[UIBarButtonItem alloc] initWithTitle:@"播放" style:UIBarButtonItemStyleDone target:self action:@selector(playBtnDidTouch)];
+    
+    self.playBtn = [[UIBarButtonItem alloc] initWithTitle:@"播放" style:UIBarButtonItemStyleDone target:self action:@selector(playBtnDidTouch:)];
+    
+    self.timerBtn = [[UIBarButtonItem alloc] initWithTitle:@"定时器" style:UIBarButtonItemStyleDone target:self action:@selector(timerBtnDidTouch:)];
+    
+    UIBarButtonItem *flexible = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
+    
+    NSArray *toolBarItems = [NSArray arrayWithObjects:self.saveBtn,flexible,self.playBtn,flexible,self.timerBtn,nil];
+    //ToolBar的创建
+//    self.toolBar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 366 + 54, 320, 50)];
+    self.toolBar = [[UIToolbar alloc] init];
+    [self.toolBar setBarStyle:UIBarStyleBlackOpaque];
+    [self.view addSubview:self.toolBar];
+    [self.toolBar mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.bottom.equalTo(self.view.mas_bottom);
+        make.size.mas_equalTo(CGSizeMake(self.view.frame.size.width, 64.0));
+    }];
+    
+
+    [self.toolBar setItems:toolBarItems];
+
 }
 
 //保存按钮点击的处理函数书
 -(void)saveBtnDidTouch:(id *)sender
 {
-    NSLog(@"保存按钮被电击了");
+    //保存一个数据到用户的本地
+    
+    
+    //有一个弹窗告诉用户已经完成了保存
 }
 
 //播放按钮被点击
 -(void)playBtnDidTouch:(id *)sender
 {
-    NSLog(@"播放按钮被点击了");
+    //播放当前选中的音轨
+}
+
+//定时器按钮点击的处理函数
+-(void)timerBtnDidTouch:(id *)sender
+{
+    
 }
 
 /*
---
+++ Podfile
@@ -0,0 +1,4 @@
+platform :ios,'8.0'
+target 'Nozic' do
+   pod ‘Masonry’
+end
